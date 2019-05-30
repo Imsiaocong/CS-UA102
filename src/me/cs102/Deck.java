52224261751aac6109966ee3c1eba6fe9926ff18
王笛 Wang,Di <wd1998313@gmail.com>
2019-05-29 20:52:35 -0400

    updated

--- src/me/cs102/Deck.java
+++ src/me/cs102/Deck.java
@@ -4,21 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck <A> {
-	//private Random shuffle = new Random();
-	static ArrayList<NYUStudent> students = new ArrayList<NYUStudent>();
-	public static void createItem(String sname, String sid) {
-		NYUStudent student = new NYUStudent(sname,sid);
-		students.add(student);
+	ArrayList<A> students;
+	public Deck(ArrayList<A> sstudent) {
+		students = sstudent;
 	}
 	
-	public static void main(String[] args) {
-		createItem("Di Wang", "666");
-		createItem("Di Wang2", "777");
-		createItem("Di Wang3", "888");
-		createItem("Di Wang4", "999");
-		createItem("Di Wang5", "000");
-		System.out.println(students);
-		Collections.shuffle(students);
-		System.out.println(students);
+	public ArrayList<A> deck_shuffle(ArrayList<A> lst) {
+		ArrayList<A> lst_new = new ArrayList<A>();
+		Collections.shuffle(lst_new);
+		return lst_new;
 	}
+	
 }
---
+++ src/me/cs102/StudentDeck.java
@@ -0,0 +1,21 @@
+package me.cs102;
+
+import java.util.ArrayList;
+
+public class StudentDeck {
+	public static void main(String args[]) {
+		NYUStudent a = new NYUStudent("D W","222");
+		NYUStudent b = new NYUStudent("D W","333");
+		NYUStudent c = new NYUStudent("D W","444");
+		NYUStudent d = new NYUStudent("D W","555");
+		NYUStudent e = new NYUStudent("D W","666");
+		ArrayList<NYUStudent> students = new ArrayList<NYUStudent>();
+		students.add(a);
+		students.add(b);
+		students.add(c);
+		students.add(d);
+		students.add(e);
+		Deck<NYUStudent> studentDeck = new Deck<NYUStudent>(students);
+		System.out.println(studentDeck);
+	}
+}
